c08452b834b3a3912bdd316205989a246717627c
Harsh <161842091+Harsh-Nainuji@users.noreply.github.com>
2025-05-17 12:10:04 +0530

    Update adminApi.js

--- admin/src/services/adminApi.js
+++ admin/src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const adminApi = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://coffe-shop-backend.onrender.com',
   headers: {
     Authorization: `Basic ${btoa('admin:admin123')}`
   }
