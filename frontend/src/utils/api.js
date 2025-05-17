06acea0c149d5d970945eb366a307594a6305fb7
Harsh <161842091+Harsh-Nainuji@users.noreply.github.com>
2025-05-17 12:08:13 +0530

    livelink api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://coffe-shop-backend.onrender.com' });
 export default api;
 //comment
